bf80b1e10fa9fe25bb2cfad1da6ca51aa26d6867
Benjamin Höglinger-Stelzer <benjamin.hoeglinger@nefarius.at>
2026-02-21 17:03:18 +0100

    Update run_demos.cmd

--- run_demos.cmd
+++ run_demos.cmd
@@ -9,6 +9,8 @@ set INJECT_X86=".\bin\x86\Injector.exe"
 set IMGUI_LIB="HydraHook-ImGui.dll"
 
 %INJECT_X64% -n BEHEMOTH.exe -i %IMGUI_LIB%
+%INJECT_X64% -n Ryse.exe -i %IMGUI_LIB%
+%INJECT_X64% -n RoadRedemption.exe -i %IMGUI_LIB%
 %INJECT_X86% -n hl2.exe -i %IMGUI_LIB%
 %INJECT_X86% -n Bioshock2.exe -i %IMGUI_LIB%
 
