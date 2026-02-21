89f6742c92c3696f7e93ac9cc2789c59ada5f7bd
Benjamin Höglinger-Stelzer <benjamin.hoeglinger@nefarius.at>
2026-02-21 16:26:54 +0100

    Create run_demos.cmd

---
+++ run_demos.cmd
@@ -0,0 +1,16 @@
+@echo off
+@setlocal
+
+set MYDIR=%~dp0
+pushd "%MYDIR%"
+
+set INJECT_X64=".\bin\x64\Injector.exe"
+set INJECT_X86=".\bin\x86\Injector.exe"
+set IMGUI_LIB="HydraHook-ImGui.dll"
+
+%INJECT_X64% -n BEHEMOTH.exe -i %IMGUI_LIB%
+%INJECT_X86% -n hl2.exe -i %IMGUI_LIB%
+%INJECT_X86% -n Bioshock2.exe -i %IMGUI_LIB%
+
+popd
+endlocal
